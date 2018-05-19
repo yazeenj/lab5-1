c5719332e1cdd3a66f909c076ac3db0fa3f7384d
Frida <didamalm@gmail.com>
2018-05-19 17:51:04 +0200

    Implementation of user story 1. Frame

---
+++ BowlingCalculator/src/Frame.java
@@ -0,0 +1,32 @@
+public class Frame {
+    private int[] throwScores;
+    private int currentThrow;
+
+    public Frame() {
+        this.throwScores = new int[2];
+        this.currentThrow = 0;
+    }
+
+    public void addThrow(int throwScore) {
+        if (this.currentThrow < 2) {
+            this.throwScores[currentThrow] = throwScore;
+            this.currentThrow++;
+        }
+    }
+
+    public int getFirstThrow() {
+        int returnValue = -1;
+        if (this.currentThrow > 0) {
+            returnValue = this.throwScores[0];
+        }
+        return returnValue;
+    }
+
+    public int getSecondThrow() {
+        int returnValue = -1;
+        if (this.currentThrow > 1) {
+            returnValue = this.throwScores[1];
+        }
+        return returnValue;
+    }
+}

5d03e1593d84853a885f34dbfa16dd3d3734be8d
Frida <didamalm@gmail.com>
2018-05-19 17:09:59 +0200

    Test case for user story 1. Frame

--- BowlingCalculator/tests/MainTest.java
+++ BowlingCalculator/tests/MainTest.java
@@ -1,5 +1,23 @@
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class MainTest {
+
+    /**
+     *      1. Frame
+     */
+
+    @Test
+    public void frame() {
+        Frame frame = new Frame();
+        frame.addThrow(3);
+        frame.addThrow(5);
+        int firstThrow = frame.getFirstThrow();
+        int secondThrow = frame.getSecondThrow();
+        assertEquals(3, firstThrow);
+        assertEquals(5, secondThrow);
+    }
+
 
 }

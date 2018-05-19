cc1d9d0bded9abcf70e84297d804e96170f6fab6
yazeenj <yazeenj@outlook.com>
2018-05-19 21:03:29 +0200

    Implementation of user story 4. Game Score

--- BowlingCalculator/src/Game.java
+++ BowlingCalculator/src/Game.java
@@ -3,9 +3,31 @@ public class Game {
 
     public Game() {
         frames = new Frame[10];
+
+        //array init
+        for(int i=0; i< this.frames.length; i++){
+            this.frames[i] = new Frame();
+        }
     }
 
     public Frame[] getAllFrames() {
         return frames;
+    }
+
+    public int getGameScore(){
+        int gameScore = 0;
+        for(int i=0; i < this.frames.length;i++){
+            gameScore +=this.frames[i].countTotalScore();
+        }
+        return gameScore;
+    }
+
+    public void setGameFrameScore(int [] arr){
+        int arrCount = 0;
+        for(int i=0; i< this.frames.length; i++){
+            this.frames[i].addThrow(arr[arrCount]);
+            this.frames[i].addThrow(arr[arrCount+1]);
+            arrCount+=2;
+        }
     }
 }

4fc43701008dd0a124cd2e186e511e6d4e22f638
Frida <didamalm@gmail.com>
2018-05-19 18:12:02 +0200

    Implementation of user story 3. Game

---
+++ BowlingCalculator/src/Game.java
@@ -0,0 +1,11 @@
+public class Game {
+    private Frame[] frames;
+
+    public Game() {
+        frames = new Frame[10];
+    }
+
+    public Frame[] getAllFrames() {
+        return frames;
+    }
+}

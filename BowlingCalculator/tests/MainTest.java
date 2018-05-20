f43ae2381ae4b42518033928334f7b93e9d58c5d
Mikael Richardsson <mickerich96@gmail.com>
2018-05-20 17:48:24 +0200

    Test case for user story 10. Spares as the last frame

--- BowlingCalculator/tests/MainTest.java
+++ BowlingCalculator/tests/MainTest.java
@@ -145,4 +145,24 @@ public class MainTest {
         assertEquals(98, result);
     }
 
+    /**
+     *      10. Spare as a last frame
+     */
+    @Test
+    public void spareAsALastFrame() {
+        Game game = new Game();
+        int [] arr = {1, 5, 3, 6, 7, 2, 3, 6, 4, 4, 5, 3, 3, 3, 4, 5, 8, 1, 2, 6};
+        int[] scorearr = {0, 0};
+        int bonus = 7;
+        for(int i = 0; i < 10; i++) {
+            scorearr[0] = arr[2 * i];
+            scorearr[1] = arr[2 * i + 1];
+            game.setGameRoundFrameScore(scorearr);
+            game.setGameRoundBonusScore(bonus);
+        }
+        int result = game.getGameScore();
+        assertEquals(98, result);
+
+    }
+
 }

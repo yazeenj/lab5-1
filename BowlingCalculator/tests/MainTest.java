32cc47a681c7d158ebc54fac5e36f738e97b0031
Mikael Richardsson <mickerich96@gmail.com>
2018-05-20 15:13:24 +0200

    Test case for user story 6. Spare

--- BowlingCalculator/tests/MainTest.java
+++ BowlingCalculator/tests/MainTest.java
@@ -77,4 +77,21 @@ public class MainTest {
         assertEquals(94, result);
     }
 
+    /**
+     *      6. Spare
+     */
+    @Test
+    public void spare() {
+        Game game = new Game();
+        int [] arr = {1, 9, 3, 6, 7, 2, 3, 6, 4, 4, 5, 3, 3, 3, 4, 5, 8, 1, 2, 6};
+        int[] scorearr = {0, 0};
+        for(int i = 0; i < 10; i++) {
+            scorearr[0] = arr[2 * i];
+            scorearr[1] = arr[2 * i + 1];
+            game.setGameRoundFrameScore(scorearr);
+        }
+        int result = game.getGameScore();
+        assertEquals(88, result);
+    }
+
 }
